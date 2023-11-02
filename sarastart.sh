4c2b51844712d87ea1f6f8a5bfead6e981e0848b
gustavoduqueo <gustavoduqueo@gmail.com>
2023-11-01 20:45:45 -0500

    Update sarastart.sh

--- sarastart.sh
+++ sarastart.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# Create Python virtualenv
+# Create Python virtualenv and SARA jam project
 python3 -m virtualenv /opt/SARA/
-. /opt/SARA/bin/activate
-# Create SARA jam project
-/opt/SARA/bin/python3 /opt/SARA/bin/jam-project.py
+. /opt/SARA/bin/activate && /opt/SARA/bin/python3 /opt/SARA/bin/jam-project.py
 # Run SARA server
 /opt/SARA/bin/python3 /opt/SARA/server.py
